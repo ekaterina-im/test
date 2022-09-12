326c312e1b22b4fb54318f145b4d267dd47870bb
ekaterina-im <113369132+ekaterina-im@users.noreply.github.com>
2022-09-12 18:42:38 +0300

    sun

--- document.py
+++ document.py
@@ -2,3 +2,4 @@ Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AM
 Type "help", "copyright", "credits" or "license()" for more information.
 >>>
 print("hello!!")
+print("sun")
